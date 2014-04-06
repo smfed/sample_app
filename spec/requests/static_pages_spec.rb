eb199af3bf59d290084e9a0989caa9e56c31a28a
Stepan Fedorov <smfedorov@gmail.com>
2014-04-06 21:27:16 +0400

    - Finish chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,34 +3,54 @@ require 'spec_helper'
 describe "StaticPages" do
   subject  { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home Page" do
     before { visit root_path }
 
-    it {should have_content('Likenstrike.Me')}
-    it {should have_title(full_title(''))}
+    let (:heading) {'Likenstrike.Me'}
+    let (:page_title) {''}
+
+    it_should_behave_like "all static pages"
     it {should_not have_title("| Home")}
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    let (:heading) {'Help'}
+    let (:page_title) {'Help'}
+
+    it_should_behave_like "all static pages"
+
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content('About') }
-    it { should have_title(full_title('About')) }
+    let (:heading) {'About'}
+    let (:page_title) {'About'}
+
+    it_should_behave_like "all static pages"
+
   end
 
   describe "Contact page" do
     before { visit contact_path }
+    let (:heading) {'Contact'}
+    let (:page_title) {'Contact'}
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    it_should_behave_like "all static pages"
   end
 
+  it "should have the right links on the layout" do
+    check_links link: "About", page: "About"
+    check_links link: "Help", page: "Help"
+    check_links link: "Contact", page: "Contact"
+    check_links link: "Home", page: ""
+    check_links link: "likenstrike.me", page: ""
+  end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,3 +6,9 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+
+def check_links(link_attributes = {})
+  visit root_path
+  click_link link_attributes[:link]
+  expect(page).to have_title(full_title(link_attributes[:page]))
+end
